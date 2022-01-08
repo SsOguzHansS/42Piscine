68d03ad9b1b1e952b3df301230740b1f9c9bdab3
oguzhan <oguz@gmail.com>
2022-01-08 12:41:34 +0300

    C Code

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ogozturk <ogozturk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/08 12:08:31 by ogozturk          #+#    #+#             */
+/*   Updated: 2022/01/08 12:13:02 by ogozturk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	index;
+	int	tmp;
+
+	index = 0;
+	while (index < size / 2)
+	{
+		tmp = tab[index];
+		tab[index] = tab[size - 1 - index];
+		tab[size - 1 - index] = tmp;
+		index++;
+	}
+}
